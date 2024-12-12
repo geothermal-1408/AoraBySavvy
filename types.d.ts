1fee205fef929f1729d91ceadfa7d1680d2fd1f8
geothermal-1408 <savvy3526@gmail.com>
2024-12-12 22:28:08 +0530

    added for conciseness

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+export type Videos = {
+  tittle: string;
+  prompt: string;
+  video: string;
+  $id: string;
+  creator: {
+    username: string;
+    avatar: string;
+  };
+  thumbnail: string;
+};
