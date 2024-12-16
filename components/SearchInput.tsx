286da4b5c190922c63e27acdb9ac33a12da4f18f
geothermal-1408 <savvy3526@gmail.com>
2024-12-16 13:25:45 +0530

    modified for better reusability

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,44 +1,42 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants/index";
+import { router, usePathname } from "expo-router";
 
-type InputProps = {
-  value: string;
-  handlechange: (e: string) => void;
-  otherStyles?: string;
-  keyBoardType?: string;
-  placeholder: string;
-};
-const SearchInput: React.FC<InputProps> = ({
-  value,
-  handlechange,
-  otherStyles,
-  placeholder,
-}: InputProps): JSX.Element => {
-  const [isFocused, setisFocused] = useState(false);
+const SearchInput = ({ initialQuery = "" }: { initialQuery?: string }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <View
-        className={`w-full h-16 px-4 bg-black-100 rounded-2xl border-2  ${isFocused} ? "border-secondary-200" : "border-secondary-200" items-center flex-row`}
+    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-secondary-200">
+      <TextInput
+        className="flex-1 text-white font-psemibold text-base mt-2"
+        value={query}
+        placeholder="search for videos"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert("Please enter a search query");
+          }
+          if (pathname.startsWith("/search")) {
+            router.setParams({ query });
+          } else {
+            router.push({ pathname: "/search/[query]", params: { query } });
+          }
+        }}
       >
-        <TextInput
-          className="flex-1 text-white font-psemibold text-base"
-          value={value}
-          placeholder={placeholder}
-          placeholderTextColor="#7B7B7B"
-          onChangeText={handlechange}
-          onPress={() => setisFocused(true)}
-          onBlur={() => setisFocused(false)}
-        />
-        <TouchableOpacity>
-          <Image
-            source={icons.search}
-            className="w-6 h-6"
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
-      </View>
+        <Image source={icons.search} className="w-6 h-6" resizeMode="contain" />
+      </TouchableOpacity>
     </View>
   );
 };
