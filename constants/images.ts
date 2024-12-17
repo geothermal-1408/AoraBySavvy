e474689823b986eb35b7ed5190f0157f5ab8713d
geothermal-1408 <savvy3526@gmail.com>
2024-12-17 22:30:32 +0530

    added

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,9 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
+const profile = require("../assets/images/profile.png");
+const thumbnail = require("../assets/images/thumbnail.png");
 const cards = require("../assets/images/cards.png");
 const path = require("../assets/images/path.png");
 const logo = require("../assets/images/logo.png");
 const logoSmall = require("../assets/images/logo-small.png");
-import empty from "../assets/images/empty.png";
+const empty = require("../assets/images/empty.png");
 
 export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
